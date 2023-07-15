bfcee2239ecf18a5f6b86ce2a340a26c0f979fe0
danila <danila.leontyev@gmail.com>
2023-07-15 23:22:48 +0400

    Spaces between logos added

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-// import { useState } from 'react';
+import { useState } from 'react';
 import handShake from './assets/images/HandShake.svg';
 import questions from './assets/images/Questions.svg';
 import brain from './assets/images/Brain.svg';
@@ -21,37 +21,37 @@ function App() {
 
 
         <div className="about-paragraph">
-        <div>
+          <div>
             Почти 3 года в Faject мы занимаемся разработкой и дизайном для компаний по всему миру. Мы помогаем компаниям создавать сервисы и сайты для людей. Делаем бизнес комфортным и удобным в общении с клиентами так, чтобы это было выгодно.
           </div>
           <div style={{ display: "inline-block" }}>
             <img src={handShake} alt="Hand Shake" />
-          </div> 
+          </div>
         </div>
         <div className="about-paragraph">
-        <div>
+          <div>
             <img src={questions} alt="questions" />
           </div>
-          
+
           <div className="text-right"> В своей работе мы активно работаем с заказчиком, чтобы обеспечить наилучший результат и совместное творчество
           </div>
-         
+
         </div>
         <div className="about-paragraph">
-        <div>Для оптимизации всех процессов и экономии средств клиента на начальных этапах разработки мы используем неройнные сети. Так, мы внесколько раз ускоряем подбор стиля иллюстраций, чтобы заказчик мог выбрать, что ему больше по душе. 
+          <div>Для оптимизации всех процессов и экономии средств клиента на начальных этапах разработки мы используем неройнные сети. Так, мы внесколько раз ускоряем подбор стиля иллюстраций, чтобы заказчик мог выбрать, что ему больше по душе.
           </div>
           <div>
             <img src={brain} alt="brain" />
           </div>
         </div>
-        
+
         <div className="about-paragraph">
-        <div>
+          <div>
             <img src={peoplesActivity} alt="Peoples activity" />
           </div>
           <div className="text-right">
             На этапе разработки технического задания мы изучаем потребности и цели вашей компании. Мы предлагаем наилучшие решения, экономя средства заказчика и избавляясь от лишних функций. Наша главная задача - предоставить удобный функционал и эффективный инструмент маркетинга для вашего бизнеса.
-          </div>      
+          </div>
         </div>
         <div className='about-counters-section'>
           <div className='about-counter-box'>
@@ -72,17 +72,16 @@ function App() {
             <div>Условия & Положения</div>
             <div>Политика конфиденциальности</div>
             <div>©2018—2023 Faject</div>
-            </div>
-          
+          </div>
           <div>
             <div>мы здесь</div>
-            <div>
-              <img src={instagram} alt="instagram logo" style={{ display: "inline-block" }} />
-              <img src={telegram} alt="telegram logo" style={{ display: "inline-block" }} />
-              <img src={whatsapp} alt="whatsapp logo" style={{ display: "inline-block" }} />
+            <div style={{ display: "flex", gap: "10px" }}>
+              <img src={instagram} alt="instagram logo" />
+              <img src={telegram} alt="telegram logo" />
+              <img src={whatsapp} alt="whatsapp logo" />
 
             </div>
-            </div>
+          </div>
         </div>
       </div>
     </>
